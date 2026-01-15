e4960928a1afd09489999113a70884e9117a35df
varun05126 <malthumkarvarun@gmail.com>
2026-01-15 22:02:24 +0530

    Fix sitemap import error

--- ewaste_backend/urls.py
+++ ewaste_backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from core import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap
 
 urlpatterns = [
 
