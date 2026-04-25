034300a8ffc110370e3bf433b0756d40906212f3
Varun <varun@ewaste.dev>
2026-04-25 05:06:12 +0000

    fix: add WhiteNoiseMiddleware + CompressedManifestStaticFilesStorage to serve admin CSS in production

--- ewaste_backend/settings.py
+++ ewaste_backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 # ------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -122,6 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "core/static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ------------------------
 # Default PK
